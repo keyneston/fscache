b0f81c1b5f5478fe417d43343a3c4ae9e46b4665
Tabitha <tabitha@keyneston.com>
2021-05-10 14:59:38 +0200

    Cleanup old limits work

--- main.go
+++ main.go
@@ -3,9 +3,7 @@ package main
 import (
 	"context"
 	"flag"
-	"log"
 	"os"
-	"syscall"
 
 	"github.com/google/subcommands"
 	"github.com/keyneston/fscachemonitor/cmds/read"
@@ -26,32 +24,4 @@ func main() {
 	flag.Parse()
 	ctx := context.Background()
 	os.Exit(int(subcommands.Execute(ctx)))
-
-	//if err := setLimits(); err != nil {
-	//	log.Fatalf("Error updating limits: %v", err)
-	//}
-
-}
-
-func setLimits() error {
-	var limit syscall.Rlimit
-
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
-		return err
-	}
-
-	limit.Cur = 9999999
-	limit.Max = 9999999
-
-	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
-		return err
-	}
-
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit); err != nil {
-		return err
-	}
-
-	log.Printf("Limits: %v", limit)
-
-	return nil
 }
